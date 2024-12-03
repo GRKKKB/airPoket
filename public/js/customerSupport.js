15aac29eac29bcf56c53423924133cffa94b81df
qjacks95 <ghksdl7701@naver.com>
2024-12-03 15:33:39 +0900

    s
    s

--- public/js/customerSupport.js
+++ public/js/customerSupport.js
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
+import './ContactUs.css'; // CSS 파일을 임포트합니다.
 
 export const ContactUs = () => {
   const form = useRef();
@@ -22,16 +23,23 @@ export const ContactUs = () => {
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Phone</label>
-      <input type='tel' name="user_phone" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
+    <div className="contact-container">
+      <h1>고객지원</h1>
+      <form ref={form} onSubmit={sendEmail} className="contact-form">
+        <label>이름</label>
+        <input type="text" name="user_name" placeholder="Your name" required />
+        
+        <label>이메일 주소</label>
+        <input type="email" name="user_email" placeholder="Your email address" required />
+        
+        <label>전화번호</label>
+        <input type="tel" name="user_phone" placeholder="Your phone number" required />
+        
+        <label>문의 내용</label>
+        <textarea name="message" placeholder="Comments" required />
+        
+        <input type="submit" value="전송" className="submit-button" />
+      </form>
+    </div>
   );
 };
