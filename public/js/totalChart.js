d5733f63f82d4cfdb156fc44a9109a0c106955b1
sunwhats <khr240722@gmail.com>
2024-12-06 16:49:09 +0900

    totalChart.js 차트 변경 순위변경
    수정

--- public/js/totalChart.js
+++ public/js/totalChart.js
@@ -1,12 +1,36 @@
 
 const today = document.getElementById('today');
 
+const regionMapping = {
+    Seoul: '서울',
+    Busan: '부산',
+    Daegu: '대구',
+    Incheon: '인천',
+    Gwangju: '광주',
+    Daejeon: '대전',
+    Ulsan: '울산',
+    Gyeonggi: '경기',
+    Gangwon: '강원',
+    Chungbuk: '충북',
+    Chungnam: '충남',
+    Jeonbuk: '전북',
+    Jeonnam: '전남',
+    Gyeongbuk: '경북',
+    Gyeongnam: '경남',
+    Jeju: '제주',
+    Sejong: '세종',
+    };
+
+
+
 // 현재 날짜 표시
 function displayCurrentDate() {
     const now = new Date();
     const formattedDate = `${now.getFullYear()}-${String(now.getMonth() + 1).padStart(2, '0')}-${String(now.getDate()).padStart(2, '0')}`;
     today.textContent = `현재 날짜: ${formattedDate}`;
 }
+
+
 async function fetchData() {
     try{
         //api 호출
@@ -21,9 +45,29 @@ async function fetchData() {
         const airData = await airResponse.json(); //data3
         const rankData = await rankResponse.json(); //data4
         
+
+        rankData.forEach(item => {
+            const koreanRegion = regionMapping[item.region]; // 영어 지역 이름을 한글로 매핑
+            const regionClass = `.location-${item.region.toLowerCase()}`;
+            const mapPoint = document.querySelector(regionClass);
+            
+            if(mapPoint){
+                // 한글 이름 및 값 업데이트
+                const locationElement = mapPoint.querySelector('.location');
+
+                if (locationElement) {
+                    locationElement.textContent = koreanRegion || item.region; // 한글 이름 설정
+                  }
+            }else {
+                console.warn(`"${item.region}"에 해당하는 HTML 요소를 찾을 수 없습니다.`);
+            }
+
+        });
+
         updateChartData(metalNameData,metalData);
         updateAirChartData(airData);
         updateRankData(rankData);
+
 
     } catch (error) {
         console.error('데이터를 가져오는 중 에러 발생:', error);
@@ -152,20 +196,18 @@ function updateRankData(data4){
     console.log(scores);
 
     new Chart(barCtx, {
-        type: 'bar',
+        type: 'polarArea',
         data: {
             labels: labels,
             datasets: [{
-                label: '종합 건강 점수 : MAX=100',
-                data: scores,
-                borderColor:'#FF9364',
-                backgroundColor: '#ffaa28',
-            }]
+                label: '종합건강점수 반영 RANK',
+                data: ranking,
+                borderColor:'#FAF0AD',
+                backgroundColor: '#FFC314'
+            }],
 
         },
-
-
-
+        
 
         options: {
             plugins: {
@@ -183,15 +225,49 @@ function updateRankData(data4){
                 y: {
                     title: {
                         display: true,
-                        text: '건강 점수',
+                        text: '순위',
                     },
                     beginAtZero: true,
-                    max:100,
-                    min:80
+                    max:20,
+                    min:0,
+                    grid: {
+                        drawOnChartArea: true,
+                    },
+                    ticks:{
+                        font: {
+                            size:14,
+                        },
+                    },
+
+
                 },
+                // y1:{
+                //     title:{
+                //         display: true,
+                //         text: 'RANK',
+                //     },
+
+
+                //     type: "linear",
+                //     display:true,
+                //     position: "right",
+                //     suggestedMin:17,
+                //     suggestedMax:17,
+                    
+                //     ticks: {
+                //         stepSize: 20,
+                //         font: {
+                //         size: 15,
+                //         },
+                //     } 
+
+                // }
             },
         },
     });
+
+
+    
 }
 
 
